64d900b3da739453f6e047b57c95801e5efe0384
NathanSch25 <nathan.schober25@gmail.com>
2024-09-14 16:36:15 -0500

    Mongo seters

--- api/mongoDB.py
+++ api/mongoDB.py
@@ -1,8 +1,9 @@
 import dotenv
 import os
 from pymongo import MongoClient
-
-
+from pymongo.server_api import ServerApi
+client = MongoClient("mongodb://imaquvzf:30a5a1d1-efe0-45fd-9b7b-35ffbd487c8a@<hostname:<port>",
+    server_api=ServerApi("<Stable API version>"))
 mongoPassword = str(os.environ.get("PUBLIC_MONGODB_PWD"))
 connection_string = f"mongodb+srv://nathanschober25:{mongoPassword}@core.fs1nb.mongodb.net/"
 client = MongoClient(connection_string)
@@ -14,8 +15,7 @@ def check_hashdb(pass_hash: str):
     collection = Db.Users# Checks the user table and finds the user id of the user with the given pass_hash
     user_id = collection.find_one({"password": pass_hash})
     
-    return user_id["_id"]
-    
+    return user_id["_id"] 
 def signupdb(email: str, pass_hash: str):
     # return the status of the signup
     collection = Db.Users
@@ -29,7 +29,6 @@ def signupdb(email: str, pass_hash: str):
 
     return user_id.inserted_id, {"status": "good"}
     # Return good if the signup is successful, return bad if the signup is unsuccessful
-
 def logindb(email: str, pass_hash: str):
     collection = Db.Users
     # return the status of the login
@@ -41,3 +40,34 @@ def logindb(email: str, pass_hash: str):
         
     # return bad if the login is unsuccessful
     return {"status": "bad"}
+def mkSyllabusdb(topic: str, description: str, user_id):
+    data = {
+        "foreign key": user_id,
+        "topic": topic,
+        "description": description
+    }
+    syb = collection.insert_one(data)
+    return syb["_id"], {"status": "good"}
+def mkLecturedb(description: str, video_id: str, syllabus_id):
+    
+    data = {
+        "foreign key": syllabus_id,
+        "description": description,
+        "video_id": video_id
+    }
+    lecture = collection.insert_one(data)
+    return lecture["_id"], {"status": "good"}
+def mkQuizdb(lecture_id: str):
+    data = {
+        "foreign key": lecture_id
+    }
+    quiz = collection.insert_one(data)
+    return quiz["_id"], {"status": "good"}
+def mkQuestionb(quiz_id: str, questions: str, answers):
+    data = {
+        "foreign key": quiz_id,
+        "questions": questions,
+        "answers": answers
+    }
+    question = collection.insert_one(data)
+    return question["_id"], {"status": "good"}

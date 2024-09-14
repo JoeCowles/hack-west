ea72539e42abc8850833969219cceac1148bba90
NathanSch25 <nathan.schober25@gmail.com>
2024-09-14 14:14:37 -0500

    login functions added

--- api/index.py
+++ api/index.py
@@ -6,29 +6,59 @@ from pymongo import MongoClient
 YouTubeTranscriptApi = load_dotenv(find_dotenv("YouTubeAPI_PWD"))
 app = FastAPI()
 
-MongoPassword = os.environ.get("MONGODB_PWD")
-connection_string = "mongodb+srv://nathanschober25:{MongoPassword}@core.fs1nb.mongodb.net/?retryWrites=true&w=majority&appName=Core"
+load_dotenv()
+mongoPassword = str(os.environ.get("PUBLIC_MONGODB_PWD"))
+
+connection_string = "mongodb+srv://nathanschober25:{mongoPassword}@core.fs1nb.mongodb.net/"
 client = MongoClient(connection_string)
 
 
-def check_hash(pass_hash: str):
-    # Checks the user table and finds the user id of the user with the given pass_hash
-    return "user id"
+Db = client.Core
+collection = Db.Users
+# collections = Db.list_collection_names()
+data1 = {
+    "email": "jon22@gmail.com",
+    "password": "pass"
+    }
+collection.insert_one(data1)
 
+
+
+def check_hash(pass_hash: str):
+    collection = Db.Users# Checks the user table and finds the user id of the user with the given pass_hash
+    user_id = collection.find_one({"password": pass_hash})
+    if user_id:
+        return user_id["_id"]
+    
 
 @app.post("/signup")
 def signup(email: str, pass_hash: str):
     # return the status of the signup
-    return {"status": "ok"}
+    collection = Db.Users
+
+    data1 = {
+    "email": email, 
+    "password": pass_hash
+    }
+
+    user_id = collection.insert_one(data1)
+
+    return user_id.inserted_id, {"status": "good"}
     # Return good if the signup is successful, return bad if the signup is unsuccessful
 
 
 @app.post("/login")
 def login(email: str, pass_hash: str):
+    collection = Db.Users
     # return the status of the login
-    return {"status": "ok"}
-    # Return good if the login is successful, return bad if the login is unsuccessful
-
+    if collection.find_one({"email": email}):
+        user = collection.find_one({"email": email})
+        if user["password"] == pass_hash:
+            # Return good if the login is successful
+            return user["_id"], {"status": "good"}
+        
+    # return bad if the login is unsuccessful
+    return {"status": "bad"}
 
 @app.get("/")
 def health_check():

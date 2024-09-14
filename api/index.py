41392dea4c50fc41dccdff27006684b77959c98c
Joe Cowles <JoeCowles@users.noreply.github.com>
2024-09-14 09:25:45 -0500

    Full setup

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -6,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 def health_check():
     return {"status": "ok"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)

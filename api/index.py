c9fc741b15c1adbf8d04ef5ae4a61fbeb60bc67d
NathanSch25 <nathan.schober25@gmail.com>
2024-09-14 11:21:35 -0500

    dotenv

--- api/index.py
+++ api/index.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv, find_dotenv
 import uvicorn
-
+YouTubeTranscriptApi = load_dotenv(find_dotenv("YouTubeAPI_PWD"))YouTubeAPI_PWD
 app = FastAPI()
 
+@app.get("/GetTranscript")
+def GetTranscript(video_id):
+    transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=["ar"])
 
 @app.get("/")
 def health_check():
